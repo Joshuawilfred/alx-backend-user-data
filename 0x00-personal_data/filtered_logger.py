acca1be7cc620821fee4bb5e17a6107c6d72dfd9
Joshuawilfred <joewilfred216@gmail.com>
2025-01-17 03:14:21 +0300

    Task[5]: Conn with db

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python3
+"""Personal data Task"""
+import logging
+import os
+from typing import Tuple, List
+import re
+import mysql.connector
+from mysql.connector.connection import MySQLConnection
+
+PII_FIELDS: Tuple = ("name", "email", "phone", "ssn", "password")
+
+
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
+    """Function which returns the log message obfuscated"""
+    return re.sub(r'(\w+)=([^{}]+)'.format(separator),
+                  lambda match: '{}={}'.format(match.group(1), redaction)
+                  if match.group(1) in fields
+                  else '{}={}'.format(match.group(1), match.group(2)), message)
+
+
+def get_logger() -> logging.Logger:
+    """Function which returns a logging.Logger object."""
+    logger = logging.getLogger("user-data")
+    logger.propagate = False
+
+    handler = logging.StreamHandler()
+    handler.setFormatter(RedactingFormatter(PII_FIELDS))
+
+    logger.setLevel(logging.INFO)
+    logger.addHandler(handler)
+
+    return logger
+
+
+def get_db() -> MySQLConnection:
+    """function that returns a connector to the database"""
+    db = mysql.connector.connect(
+        host=os.getenv("PERSONAL_DATA_DB_HOST", "localhost"),
+        port=3306,
+        user=os.getenv("PERSONAL_DATA_DB_USERNAME", "root"),
+        password=os.getenv("PERSONAL_DATA_DB_PASSWORD", ""),
+        database=os.getenv("PERSONAL_DATA_DB_NAME")
+    )
+    return db
+
+
+def main() -> None:
+    """Main function"""
+    logger = get_logger()
+    db = get_db()
+    cursor = db.cursor()
+
+    cursor.execute("SELECT * FROM users;")
+    users = cursor.fetchall()
+    column_names = cursor.column_names
+
+    for user in users:
+        formated_user = "".join(f"{attribute}={value}; " for
+                                attribute, value in zip(column_names, user))
+        logger.info(formated_user)
+    cursor.close()
+    db.close()
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        """Init function"""
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """format function"""
+        msg = logging.Formatter(self.FORMAT).format(record)
+        return filter_datum(self.fields, self.REDACTION, msg, self.SEPARATOR)
